92af811b59941e4f226a1f87b1c04ff01c2ef7e9
Chris McMenomy & Desmond Naranjo <pair+caristopmer.dhnaranjo@devbootcamp.com>
2017-07-28 15:31:57 -0500

    Working in cahoots with emoj properly, getting response back.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,10 +1,24 @@
 class TestsController < ApplicationController
+  require 'uri'
+
+
+
   def test
     render json: { howdy: "hello there!"}
   end
 
   def posty
+    string = "I am super happy today. Yeah!"
+    request_url = URI.encode("https://www.emoj.ai/api/classify?text=#{string}&token=#{ENV["EMOJ_ACCESS_TOKEN"]}")
     post = params[:content]
-    render json: { hello: post }
+    response = HTTParty.get(request_url)
+
+    render json: { response: response }
   end
+
+
+
+
+
+
 end
