164c38c8ceb223f75af210135d3820b9af505a7c
Chris McMenomy & Kristina Vragovic <pair+caristopmer.kvrag@devbootcamp.com>
2017-07-31 12:17:35 -0500

    emotes controller return whomp if no responses are left.

--- app/controllers/emotes_controller.rb
+++ app/controllers/emotes_controller.rb
@@ -1,20 +1,21 @@
 class EmotesController < ApplicationController
-    require 'uri'
+  require 'uri'
 
-    def create
-        emote = Emote.new(input: params[:emote])
-        #emote.user = User.new
-        emote.emotion = emote.retrieve_emotion
-        emote.actions_array = emote.retrieve_actions
-        emote.save
-        render json: { action: emote.actions_array.pop, emote_id: emote.id }
-    end
+  def create
+      emote = Emote.new(input: params[:emote])
+      #emote.user = User.new
+      emote.emotion = emote.retrieve_emotion
+      emote.actions_array = emote.retrieve_actions
+      emote.save
+      render json: { action: emote.actions_array.pop, emote_id: emote.id }
+  end
 
-    def show
-      emote = Emote.find(params[:id])
-      if emote.actions_array.empty?
-
-      end
+  def show
+    emote = Emote.find(params[:id])
+    if emote.actions_array.empty?
+      render json: { action: "whomp whomp" }
+    else
       render json: { action: emote.actions_array.pop, emote_id: emote.id }
     end
+  end
 end
