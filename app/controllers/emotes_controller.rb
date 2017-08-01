63f5154dbe72e6106574c1debe150f7829a0e78e
Chris McMenomy <chris.mcmenomy@gmail.com>
2017-08-01 17:00:30 -0500

    fixed emotes controller to have the correct key for user input.

--- app/controllers/emotes_controller.rb
+++ app/controllers/emotes_controller.rb
@@ -2,7 +2,7 @@ class EmotesController < ApplicationController
     require 'uri'
 
     def create
-        emote = Emote.new(input: params[:content])
+        emote = Emote.new(input: params[:emote])
         emote.recommendation = emote.retrieve_action
         emote.save
         render json: { action: emote.recommendation.action.content }
