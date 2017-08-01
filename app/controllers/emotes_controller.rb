2a5b180ecdf73e4ceb8c392a4868aceca737df17
Desmond Naranjo <dhnaranjo@gmail.com>
2017-08-01 12:20:51 -0500

    Updated controller for changes to emote model

--- app/controllers/emotes_controller.rb
+++ app/controllers/emotes_controller.rb
@@ -2,23 +2,12 @@ class EmotesController < ApplicationController
   require 'uri'
 
   def create
-      emote = Emote.new(input: params[:emote])
-      #emote.user = User.new
-      emote.emotion = emote.retrieve_emotion
-      emote.actions_array = emote.retrieve_actions
-      suggestion = emote.actions_array.pop
-      emote.save
-      render json: { action: suggestion, emote_id: emote.id }
+      emote = Emote.create(emote: params[:emote])
+      render json: { action: emote.retrieve_action.action, emote_id: emote.id }
   end
 
   def show
     emote = Emote.find(params[:id])
-    if emote.actions_array.empty?
-      render json: { action: "whomp whomp" }
-    else
-      suggestion = emote.actions_array.pop
-      emote.save
-      render json: { action: suggestion, emote_id: emote.id }
-    end
+    render json: { action: emote.retrieve_action.action, emote_id: emote.id }
   end
 end
