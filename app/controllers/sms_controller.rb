d73eabe0f608813c5fb0b986156ec05ea0489af8
Chris McMenomy <chris.mcmenomy@gmail.com>
2017-08-02 11:50:46 -0500

    SMS sending emote response

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,12 +1,21 @@
-require 'twilio_service'
-
 class SmsController < ApplicationController
 
   def receive
+    emote = Emote.create(emote: params[:Body])
+    account_sid = ENV["TWILIO_ACCOUNT_SID"]
+    auth_token = ENV["TWILIO_AUTH_TOKEN"]
 
+    Twilio.configure do |config|
+    config.account_sid = account_sid
+    config.auth_token = auth_token
+    end
+
+    @client = Twilio::REST::Client.new
+    @client.api.account.messages.create(
+      from: params[:To],
+      to: params[:From],
+      body: emote.retrieve_action.action
+    )
   end
-
-
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   get "/emotes/:id" => 'emotes#show'
 
   post '/sms' => 'sms#receive'
-
 end
