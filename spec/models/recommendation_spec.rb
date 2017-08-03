2cdebf5f807a4ff84bc2f5ab632da991d23e2402
Chris McMenomy <chris.mcmenomy@gmail.com>
2017-08-02 19:12:31 -0500

    fix some formatting

--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Recommendation do
   let(:action) { Action.new(action: "Mow your lawn (joy)") }
   let(:recommendation) { Recommendation.new(emote: emote, action: action) }
 
-    it "has an associated emotion" do
-        expect(recommendation.emote).to be emote
-    end
+  it "has an associated emotion" do
+    expect(recommendation.emote).to be emote
+  end
 
-    it "has an associated action" do
-        expect(recommendation.action).to be action
-    end
+  it "has an associated action" do
+    expect(recommendation.action).to be action
+  end
 end
